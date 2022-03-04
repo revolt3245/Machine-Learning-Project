78f8742c8a801911875b29678933086cbf5e60ab
revolt3245 <44830134+revolt3245@users.noreply.github.com>
2022-03-04 18:02:40 +0900

    Change parent class of Linear(Layer --> Learnable)

--- Machine Learning Project/Linear.cpp
+++ Machine Learning Project/Linear.cpp
@@ -3,9 +3,13 @@
 Eigen::MatrixXd Linear::forward(Eigen::MatrixXd panIn)
 {
     auto batchSize = panIn.rows();
-    auto panOut = panIn * this->weight + Eigen::MatrixXd::Ones(batchSize, 1) * this->bias;
+    auto Param = this->getParam();
+    auto Grad = this->getGrad();
+    //auto panOut = panIn * this->weight + Eigen::MatrixXd::Ones(batchSize, 1) * this->bias;
+    auto panOut = panIn * (*Param[0]) + Eigen::MatrixXd::Ones(batchSize, 1) * (*Param[1]);
 
-    this->weightGrad = panIn.transpose();
+    //this->weightGrad = panIn.transpose();
+    (*Grad[0]) = panIn.transpose();
     return panOut;
 }
 
@@ -19,25 +23,33 @@ ostream& Linear::printConfig(ostream& os)
 Eigen::MatrixXd Linear::backward(Eigen::MatrixXd preDiff)
 {
     auto batchSize = preDiff.rows();
-    this->biasGrad = Eigen::MatrixXd::Ones(1, batchSize) * preDiff;
+    auto Grad = this->getGrad();
+    auto Param = this->getParam();
+    //this->biasGrad = Eigen::MatrixXd::Ones(1, batchSize) * preDiff;
+    (*Grad[1]) = Eigen::MatrixXd::Ones(1, batchSize) * preDiff;
 
-    this->weightGrad *= preDiff;
+    //this->weightGrad *= preDiff;
+    (*Grad[0]) *= preDiff;
 
-    return preDiff * this->weight.transpose();
+    return preDiff * (*Param[0]).transpose();
 }
 
 void Linear::step(double learningRate)
 {
-    this->weight -= learningRate * (1e-5 * this->weight + this->weightGrad);
-    this->bias -= learningRate * this->biasGrad;
+    auto Param = this->getParam();
+    auto Grad = this->getGrad();
+    //this->weight -= learningRate * (1e-5 * this->weight + this->weightGrad);
+    //this->bias -= learningRate * this->biasGrad;
+    (*Param[0]) -= learningRate * (1e-5 * (*Param[0]) + (*Grad[0]));
+    (*Param[1]) -= learningRate * (*Grad[1]);
 }
 
 Eigen::MatrixXd Linear::getWeight()
 {
-    return this->weight;
+    return *(this->getParam()[0]);
 }
 
 Eigen::MatrixXd Linear::getBias()
 {
-    return this->bias;
+    return *(this->getParam()[1]);
 }
