8960e3b9a0c7f90b09a95d59b31a4f90e8039497
revolt3245 <44830134+revolt3245@users.noreply.github.com>
2022-03-04 18:10:08 +0900

    Delete Comment

--- Machine Learning Project/Linear.cpp
+++ Machine Learning Project/Linear.cpp
@@ -4,11 +4,9 @@ Eigen::MatrixXd Linear::forward(Eigen::MatrixXd panIn)
 {
     auto batchSize = panIn.rows();
     auto Param = this->getParam();
-    auto Grad = this->getGrad();
-    //auto panOut = panIn * this->weight + Eigen::MatrixXd::Ones(batchSize, 1) * this->bias;
+    auto Grad = this->getGrad(); * this->bias;
     auto panOut = panIn * (*Param[0]) + Eigen::MatrixXd::Ones(batchSize, 1) * (*Param[1]);
 
-    //this->weightGrad = panIn.transpose();
     (*Grad[0]) = panIn.transpose();
     return panOut;
 }
@@ -25,10 +23,8 @@ Eigen::MatrixXd Linear::backward(Eigen::MatrixXd preDiff)
     auto batchSize = preDiff.rows();
     auto Grad = this->getGrad();
     auto Param = this->getParam();
-    //this->biasGrad = Eigen::MatrixXd::Ones(1, batchSize) * preDiff;
     (*Grad[1]) = Eigen::MatrixXd::Ones(1, batchSize) * preDiff;
 
-    //this->weightGrad *= preDiff;
     (*Grad[0]) *= preDiff;
 
     return preDiff * (*Param[0]).transpose();
@@ -38,8 +34,6 @@ void Linear::step(double learningRate)
 {
     auto Param = this->getParam();
     auto Grad = this->getGrad();
-    //this->weight -= learningRate * (1e-5 * this->weight + this->weightGrad);
-    //this->bias -= learningRate * this->biasGrad;
     (*Param[0]) -= learningRate * (1e-5 * (*Param[0]) + (*Grad[0]));
     (*Param[1]) -= learningRate * (*Grad[1]);
 }
