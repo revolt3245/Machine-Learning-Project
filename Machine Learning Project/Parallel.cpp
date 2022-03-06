059f052ef60449c42b8f3a4697cf11db3c5ebe8b
revolt3245 <44830134+revolt3245@users.noreply.github.com>
2022-03-06 23:20:22 +0900

    Edit Parallel class

--- Machine Learning Project/Parallel.cpp
+++ Machine Learning Project/Parallel.cpp
@@ -1,1 +1,38 @@
 #include "Parallel.h"
+
+Eigen::MatrixXd Parallel::forward(Eigen::MatrixXd panIn)
+{
+	return Eigen::MatrixXd();
+}
+
+Eigen::MatrixXd Parallel::backward(Eigen::MatrixXd preDiff)
+{
+	return Eigen::MatrixXd();
+}
+
+ostream& Parallel::printConfig(ostream& os)
+{
+	os << this->getName() << " (\n";
+	for (auto l : this->Layers) {
+		l->printConfig(os, 1);
+		os << "\n";
+	}
+	os << ")";
+
+	return os;
+}
+
+ostream& Parallel::printConfig(ostream& os, unsigned int level)
+{
+	for (auto i = 0; i < level; i++) {
+		os << "    ";
+	}
+	os << this->getName() << " (\n";
+	for (auto l : this->Layers) {
+		l->printConfig(os, level + 1);
+		os << "\n";
+	}
+	os << ")";
+
+	return os;
+}
--- Machine Learning Project/Parallel.h
+++ Machine Learning Project/Parallel.h
@@ -10,5 +10,19 @@ using namespace std;
 class Parallel :
     public Learnable
 {
-};
+public:
+    Parallel(vector<Layer*> Layers) 
+        :Learnable("Parallel"), Layers(Layers), ForkMapping(), JoinMapping() {};
+    Parallel(vector<Layer*> Layers, vector<Eigen::MatrixXd> ForkMapping, vector<Eigen::MatrixXd> JoinMapping)
+        :Learnable("Parallel"), Layers(Layers), ForkMapping(ForkMapping), JoinMapping(JoinMapping) {};
 
+    virtual Eigen::MatrixXd forward(Eigen::MatrixXd panIn) override;
+    virtual Eigen::MatrixXd backward(Eigen::MatrixXd preDiff) override;
+    virtual ostream& printConfig(ostream& os) override;
+    virtual ostream& printConfig(ostream& os, unsigned int level) override;
+
+private:
+    vector<Layer*> Layers;
+    vector<Eigen::MatrixXd> ForkMapping;
+    vector<Eigen::MatrixXd> JoinMapping;
+};
